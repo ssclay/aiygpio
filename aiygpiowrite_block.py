8ce059a4998c6a133dbab275ec9f1c46f0ccd2b7
Scott Clay <sclay@n.io>
2018-05-21 23:20:36 +0000

    add selectproperty, all pins

--- aiygpiowrite_block.py
+++ aiygpiowrite_block.py
@@ -1,19 +1,30 @@
+from enum import Enum
 from nio.block.base import Block
-from nio.properties import VersionProperty, BoolProperty
-from aiy.pins import PIN_A
+from nio.properties import VersionProperty, SelectProperty, Property
+from aiy.pins import *
 from gpiozero import LED
 
+
+class PinSelection(Enum):
+
+    PIN_A = PIN_A
+    PIN_B = PIN_B
+    PIN_C = PIN_C
+    PIN_D = PIN_D
 
 class Aiygpiowrite(Block):
 
     version = VersionProperty('0.1.0')
-    value = BoolProperty(title='Pin State', default='{{$value}}')
+    value = Property(title='Pin State', default='{{$value}}')
+    aiy_pin = SelectProperty(PinSelection,
+                             title="Pin Selection",
+                             default=PinSelection.PIN_A)
 
     def start(self):
         super().start()
-        self.pin = LED(PIN_A)
+        self.pin = LED(self.aiy_pin().value)
 
     def process_signals(self, signals):
         for signal in signals:
-            self.pin(self.value(signal))
+            self.pin.value = not self.value(signal)
         self.notify_signals(signals)
